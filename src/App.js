3658dcf02e7a1d68b137a9f61a096b066568759f
Rick Brown <ricky.brown.00@gmail.com>
2020-01-18 23:32:41 +0000

    adds scaffold

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import { useInterval } from './config/useInterval'
+import { SNAKE_START, APPLE_START, CANVAS_SIZE } from './config/constants'
 
 function App() {
-  return <div test-id="app">app</div>
+  const canvasRef = React.useRef()
+  const [gameOver, setGameOver] = React.useState()
+  const [snake, setSnake] = React.useState(SNAKE_START)
+  const [apple, setApple] = React.useState(APPLE_START)
+  const [direction, setDirection] = React.useState([0, -1])
+  const [speed, setSpeed] = React.useState(null)
+
+  const startGame = () => {}
+
+  const endGame = () => {}
+
+  const moveSnake = () => {}
+
+  const createApple = () => {}
+
+  const checkCollision = () => {}
+
+  const checkAppleCollision = () => {}
+
+  const gameLoop = () => {}
+
+  React.useEffect(() => {}, [snake, apple, gameOver])
+
+  return (
+    <div test-id="app" role="button" tabindex="0" onKeyDown={e => moveSnake(e)}>
+      <canvas
+        style={{ border: '1px solid black' }}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      {gameOver && <div>Game Over!!!!</div>}
+      <button onClick={startGame}>Start Button</button>
+    </div>
+  )
 }
 
 export default App
